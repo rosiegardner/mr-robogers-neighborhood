2154c0d02e5c87e9ad8d8cf3b72eec7a39e5cb3b
Rosie Gardner <rosiegardner78@gmail.com>
2022-02-11 13:31:12 -0800

    add styles to h2-rogers

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
   function roboBeepBoop(roboInput) {
-  let roboOutput = [];
-  for (let index = 0; index <= roboInput; index++) {
-    if (index.toString().includes("3")) {
-      roboOutput.push("Won't you be my neighbor?");
+   let roboOutput = [];
+    for (let index = 0; index <= roboInput; index++) {
+      if (index.toString().includes("3")) {
+        roboOutput.push("Won't you be my neighbor?");
+      }
+       else if (index.toString().includes("2")) {
+        roboOutput.push("Boop!");
+      }
+       else if (index.toString().includes("1")) {
+        roboOutput.push("Beep!");
+      } 
+       else {
+       roboOutput.push(" " + index);
+      }
     }
-     else if (index.toString().includes("2")) {
-      roboOutput.push("Boop!");
-    }
-     else if (index.toString().includes("1")) {
-      roboOutput.push("Beep!");
-    } 
-    else {
-      roboOutput.push(" " + index);
-    }
-  }
   return roboOutput;
 }
 
