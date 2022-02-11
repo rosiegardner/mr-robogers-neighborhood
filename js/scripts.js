d723fbd41202ab2b50cbd0680a4284a2dd26d49a
Rosie Gardner <rosiegardner78@gmail.com>
2022-02-10 17:37:18 -0800

    function passes third test

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,38 @@
 
 function roboBeepBoop(roboInput) {
   let roboOutput = [];
-  for (let i = 0; i <= roboInput; i++) {
-    if (i.toString().includes("1")); {
+  for (let index = 0; index <= roboInput; index++) {
+    if (index.toString().includes("1")) {
       roboOutput.push("Beep!");
-    } if (i.toString().includes("2")); {
-      roboOutput.push("Boop!");
-    } 
     }
-    return roboOutput;
+     else if (index.toString().includes("2")) {
+      roboOutput.push("Boop!");
+    }
+     else if (index.toString().includes("3")) {
+      roboOutput.push("Won't you be my neighbor?");
+    } else {
+      roboOutput.push(" " + index);
+    }
   }
+  return roboOutput;
+}
+     
+roboBeepBoop[0,1,2,3,4,5];
 
+
+
+  // function roboBeepBoop(roboInput) {
+  //   let roboOutput = [];
+  //   for (let i = 0; i <= roboInput; i++) {
+  //     if (i.toString().includes(1)); {
+  //       roboOutput.push("Beep!");
+  //     } 
+  //     if (i.toString().includes(2)); {
+  //       roboOutput.push("Boop!");
+  //     } 
+  //     if (i.toString().includes(3)); {
+  //       roboOutput.push("Won't you be my neighbor?");
+  //     }
+  //     }
+  //     return roboOutput;
+  //   }
