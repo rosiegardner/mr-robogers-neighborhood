d2cc6add09c70403379f8234be50370a9623779a
Rosie Gardner <rosiegardner78@gmail.com>
2022-02-10 16:36:05 -0800

    function passes second test

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,9 @@ function roboBeepBoop(roboInput) {
   for (let i = 0; i <= roboInput; i++) {
     if (i.toString().includes("1")); {
       roboOutput.push("Beep!");
-    }    
+    } if (i.toString().includes("2")); {
+      roboOutput.push("Boop!");
+    } 
     }
     return roboOutput;
   }
