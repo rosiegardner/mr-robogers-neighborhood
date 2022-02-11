c15b09169d95b86805e1ebf6cdf55fdc9638b8f1
Rosie Gardner <rosiegardner78@gmail.com>
2022-02-10 17:43:57 -0800

    function passes fourth test

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 - Test: "Numbers that contain a 1 are replaced with "Beep!"."
 - Code: roboBeepBoop();
-- Input" "1";
+- Input: 1
 - Expected Output: "Beep!"
 
 - Test: "Numbers that contain a 2 are replaced with "Beep!"."
 - Code: roboBeepBoop();
-- Input" "2";
+- Input: 2
 - Expected Output: "Boop!"
 
 - Test: "Numbers that contain a 3 are replaced with "Beep!"."
 - Code: roboBeepBoop();
-- Input" "3";
+- Input: 3
 - Expected Output: "Won't you be my neighbor?"
+
+- Test: "If a user inputs a 5, the program should display a list of values starting with 0 to the user's inputted number, with the numbers 1, 2, and 3 substituted with the correct phrase."
+- Code: roboBeepBoop();
+- Input: 5
+- Output: "0", "Beep!", "Boop", "Won't you be my neighbor?", 4, 5"
--- js/scripts.js
+++ js/scripts.js
@@ -11,29 +11,11 @@ function roboBeepBoop(roboInput) {
     }
      else if (index.toString().includes("3")) {
       roboOutput.push("Won't you be my neighbor?");
-    } else {
+    } 
+    else {
       roboOutput.push(" " + index);
     }
   }
   return roboOutput;
 }
-     
-roboBeepBoop[0,1,2,3,4,5];
 
-
-
-  // function roboBeepBoop(roboInput) {
-  //   let roboOutput = [];
-  //   for (let i = 0; i <= roboInput; i++) {
-  //     if (i.toString().includes(1)); {
-  //       roboOutput.push("Beep!");
-  //     } 
-  //     if (i.toString().includes(2)); {
-  //       roboOutput.push("Boop!");
-  //     } 
-  //     if (i.toString().includes(3)); {
-  //       roboOutput.push("Won't you be my neighbor?");
-  //     }
-  //     }
-  //     return roboOutput;
-  //   }
